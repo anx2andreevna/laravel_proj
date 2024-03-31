daabbf3b5f81383731a18e63d8b7d11934c2c9ab
Anna Tsvetkova <anx2andreevna@gmail.com>
2024-04-01 02:01:08 +0300

    lab 8

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ArticleMail;
 
 class ArticleController extends Controller
 {
@@ -43,7 +45,8 @@ class ArticleController extends Controller
         $article->shortDesc = $request->shortDesc;
         $article->desc = $request->desc;
 
-        $article->save();
+        $result = $article->save();
+        if ($result) Mail::send(new ArticleMail($article));
         return redirect(route('article.index'));
     }
 
